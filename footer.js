e7133a19114d2a1bd9322fa273b8dc3d222194a8
Orderr147 <131309176+Orderr147@users.noreply.github.com>
2023-06-18 17:23:46 +0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -25,20 +25,20 @@ let footerf=()=>{
         <li><a href="#">Cancellation and returns</a></li>
     </ul>
     <div class="subscribe">
-        <h2>Contact Us</h2> WhatsApp us : <span style="display: inline-block; margin-bottom: 1rem;"><a href="#">7000370003</a></span><br>
-        Call Us : <span style="display: inline-block;"><a href="#">1800 890 1222</a></span><br>
+        <h2>Contact Us</h2> WhatsApp us : <span style="display: inline-block; margin-bottom: 1rem;"><a href="https://www.facebook.com/manarmohamed.adel.9?mibextid=ZbWKwL">098889778</a></span><br>
+        Call Us : <span style="display: inline-block;"><a href="https://www.facebook.com/manarmohamed.adel.9?mibextid=ZbWKwL">0771222</a></span><br>
         <p>8:00 AM to 8:00 PM, 365 days</p>
-        <p>Please note that you are accessing the BETA Version of <a href="#">www.jiomart.com</a></p>
-        <p>Should you encounter any bugs, glitches, lack of functionality, delayed deliveries, billing errors or other problems on the beta website, please email us on <a href="mailto:nilmes96@gmail.com">cs@jiomart.com</a></p> <br>
+        <p>Deal with us<a href="https://www.facebook.com/manarmohamed.adel.9?mibextid=ZbWKwL">www.Manar.com</a></p>
+        <p> please email us  <a href="mailto:manaradel99760@gmail.com">manaradel99760@gmail.com</a></p> <br>
         <h2>Download App</h2> <br>
-        <a href="https://play.google.com/store/apps/details?id=com.jpl.jiomart"  style="margin-right: 16px;"><img src="https://www.jiomart.com/images/cms/wysiwyg/app-icons/play_store.png" alt="Download Jiomart App for Android from Play Store"></a>
-        <a href="https://apps.apple.com/in/app/jiomart/id1522085683"><img src="https://www.jiomart.com/images/cms/wysiwyg/app-icons/ios_store.png" alt="Download Jiomart App for iOs from App Store"></a>
+        <a href="#"  style="margin-right: 16px;"><img src="https://www.jiomart.com/images/cms/wysiwyg/app-icons/play_store.png" alt="Download Manar App for Android from Play Store"></a>
+        <a href="#"><img src="https://www.jiomart.com/images/cms/wysiwyg/app-icons/ios_store.png" alt="Download Manar App for iOs from App Store"></a>
     </div>
 </div>
 <section class="copyrights">
     <div class="copyrights-container">
-        <p>Best viewed on Microsoft Edge 81+, Mozilla Firefox 75+, Safari 5.1.5+, Google Chrome 80+</p>
-          <p> © 2022 All rights reserved. Reliance Retail Ltd.</p>
+        <p>Thank you for using Al Manar App</p>
+          <p> © 2023 All rights reserved. to Manar Adel.</p>
     </div>
 </section>`
 }
